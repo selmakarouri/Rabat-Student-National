530e2fe4943cd7a5e1feec0ec621a8b0ac2bc49f
selmakarouri <169104862+selmakarouri@users.noreply.github.com>
2024-07-24 23:21:24 +0100

    Update Rabat-Student-National.js

--- Rabat-Student-National.js
+++ Rabat-Student-National.js
@@ -557,7 +557,7 @@ const RabatLPremiumButton = createButton(" Rabat Language/selectivity  Premium",
 })();
     console.log("تم تنفيذ الشيفرة لزر CAsa reunification visa   Normal بنجاح!");
   });
-  const RabatRPremiumButton = createButton("Casa  reunification visa  Premium", function() {
+  const RabatNPremiumButton = createButton("Casa  reunification visa  Premium", function() {
 (function () {
     "use strict";
 
